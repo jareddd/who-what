a53617a7241c0661f31c1e4bd5f1b941a03bc9b9
Stout <cjstout@slc-mba001thepeilocal.local>
2016-11-07 19:41:08 -0700

    second commit

--- whowhat/ViewController.swift
+++ whowhat/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("whobudy whatuby")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
