6acf23b53b9ea93468acf97cde2c4e12d11c0e93
Stout <cjstout@slc-mba001thepeilocal.local>
2016-11-07 19:46:19 -0700

    another

--- whowhat/ViewController.swift
+++ whowhat/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("whobudy whatuby")
+        print("whobudy whatuby and then more stuff")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
